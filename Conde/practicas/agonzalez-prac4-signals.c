a3bf21637113d61ee7c4565f1aa6e4b35cddd17c
Andres Gonzalez Conde <andres.gonconde@gmail.com>
2018-07-03 21:57:37 -0500

    updated signals

--- Conde/practicas/agonzalez-prac4-signals.c
+++ Conde/practicas/agonzalez-prac4-signals.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #define EVENT_1 (1)
 #define EVENT_2 (2)
@@ -9,28 +10,67 @@
 
 uint8_t flag;
 
-void sig_handlr(int signo)
+void sig_handler(int signo)
 {
 	if(signo == SIGINT)
 	{
-		//flag = (1<<EVENT_1);
+		flag |= (1<<EVENT_1);
 	}
-	if(signo == SIGTERM)
+	else if(signo == SIGUSR1)
 	{
-		printf("Terminating process");
+		flag |=(1<<EVENT_2);
 	}
+	else if(signo == SIGTERM)
+	{
+		flag |= (1<<EVENT_3);
+		//printf("Terminating process");
+	}
+}
+
+void miFunc()
+{
+	printf("flag = 0x%x\n", flag);
+	flag = 0;
+	exit(0);
 }
 
 int main(void)
 {
 	//uint8_t counter;
-	if(signal(SIGTERM, sig_handlr)==SIG_ERR)
+	if(signal(SIGINT, sig_handler)==SIG_ERR)
+	{
+		printf("\n Can't catch SIGINT");
+	}
+	else
+	{
+		printf("\nISR installed for SIGINT\n");
+	}
+	if(signal(SIGTERM, sig_handler)==SIG_ERR)
 	{
 		printf("\n Can't catch SIGTERM");
 	}
-	//flag = 0;
+	else
+	{
+		printf("\nISR installed for SIGTERM\n");
+	}
+	if(signal(SIGUSR1, sig_handler)==SIG_ERR)
+	{
+		printf("\n Can't catch SIGUSR1");
+	}
+	else
+	{
+		printf("\nISR installed for SIGUSR1\n");
+	}
+	flag = 0;
 	while(1)
 	{
-		sleep(1);
+		if(flag)
+		{
+			miFunc();
+		}
+		else
+		{
+			sleep(1);
+		}
 	}
 }
